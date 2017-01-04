2c741c1d23a42db46fa28f7c315458f0c122c4c3
Julia Kudryavtseva <juliet.kudryavtseva@gmail.com>
2017-01-03 20:18:12 -0500

    Install Paperclip

--- Gemfile
+++ Gemfile
@@ -14,6 +14,8 @@ gem 'puma', '~> 3.0'
 gem 'sass-rails', '~> 5.0'
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
 # gem 'bootstrap_form'
+# It's an easy file attachment library for ActiveRecord
+gem 'paperclip', '~> 5.1'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170103234951) do
+ActiveRecord::Schema.define(version: 20170104005308) do
 
   create_table "pins", force: :cascade do |t|
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["user_id"], name: "index_pins_on_user_id"
   end
 
