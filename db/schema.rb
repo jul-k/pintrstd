a9c46acff256fe23b711db695c5c39e4a69e057a
Julia Kudryavtseva <juliet.kudryavtseva@gmail.com>
2017-01-03 19:19:53 -0500

    Associate Pins with Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170103001029) do
+ActiveRecord::Schema.define(version: 20170103234951) do
 
   create_table "pins", force: :cascade do |t|
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_pins_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
