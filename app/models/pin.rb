7180a6bd2b04c4367940d00fe651b2070b6d8b56
Julia Kudryavtseva <juliet.kudryavtseva@gmail.com>
2017-01-06 22:39:16 -0500

    Add a Pin validation

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,6 @@ class Pin < ApplicationRecord
     belongs_to :user
     has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+    validates_attachment :image, presence: true, size: { in: 0..3.megabytes }
+    validates :description, presence: true, length: {maximum: 200}
 end
